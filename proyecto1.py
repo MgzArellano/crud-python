b683e02b4122ccb1f2b067ec5469241822840c57
Marco Arellano <mgz.arellano@gmail.com>
2022-01-10 20:00:22 -0600

    En la historia regresando a 'Otra nueva línea'

--- proyecto1.py
+++ proyecto1.py
@@ -2,9 +2,6 @@ def saludo():
     print("Hola mudo!, desde la funcion Saludo.")
     print('\n')
     print("Otra nueva linea")
-    print('Marco')
-    print('Juliette')
-    print('Valeria')
 
 
 if __name__ == '__main__':
