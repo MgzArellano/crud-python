86e96c7396dcb7244fb80d9e65fae1e872d90d2b
Marco Arellano <mgz.arellano@gmail.com>
2022-01-10 20:01:35 -0600

    En la historia regresando a 'Adding Valeria'

--- proyecto1.py
+++ proyecto1.py
@@ -2,6 +2,9 @@ def saludo():
     print("Hola mudo!, desde la funcion Saludo.")
     print('\n')
     print("Otra nueva linea")
+    print('Marco')
+    print('Juliette')
+    print('Valeria')
 
 
 if __name__ == '__main__':
